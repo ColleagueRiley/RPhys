87dd1449543a83432e2e0ba61768c2c7f7ae53cf
Colleague Riley <colleagueRiley@gmail.com>
2023-12-04 19:40:17 -0500

    updated example

--- example/main.c
+++ example/main.c
@@ -10,15 +10,15 @@ int main() {
     RPhys_init();
 
     RPhys_body bodies[] = {
-            {RPhys_shape_loadRect(RSGL_RECT(200, 20, 20, 20), 0.90f), false},
-            {RPhys_shape_loadPolygon(RSGL_RECT(240, 20, 20, 20), 8, 9.0f), false},
-            {RPhys_shape_loadRect(RSGL_RECT(0, 200, 500, 100), 7.0f), true},
-            {RPhys_shape_loadRect(RSGL_RECT(0, 400, 500, 100), 10.0f), true}
+            {RPhys_shape_loadRect(RSGL_RECT(200, 20, 20, 20), 0.90f, ICE), false},
+            {RPhys_shape_loadPolygon(RSGL_RECT(240, 20, 20, 20), 8, 9.0f, RUBBER), false},
+            {RPhys_shape_loadRect(RSGL_RECT(0, 200, 500, 100), 7.0f, CONCRETE_DRY), true},
+            {RPhys_shape_loadRect(RSGL_RECT(20, 400, 500, 100), 10.0f, CONCRETE_DRY), true}
     };
 
     RPhys_addBodies(bodies, sizeof(bodies) / sizeof(RPhys_body));
 
-    RSGL_window* win = RSGL_createWindow("name", RSGL_RECT(0, 0, 500, 700), (u64)RSGL_CENTER);
+    RSGL_window* win = RSGL_createWindow("name", RSGL_RECT(0, 0, 700, 700), (u64)RSGL_CENTER);
 
     win->fpsCap = 60;
 
@@ -27,28 +27,35 @@ int main() {
     u32 font = RSGL_loadFont("DejaVuSansMono.ttf");
     RSGL_setFont(font);
 
+    u8 player = 0;
+
     while (running) {
         while (RSGL_window_checkEvent(win)) {
             switch (win->event.type) {
                 case RGFW_quit:
                     running = false;
                     break;
+                case RGFW_keyReleased:
+                    if (win->event.keyCode == RGFW_Space)
+                        player++;
+                    if (player >= 2)
+                        player = 0;
                 default:
                     break;
             }
         }
-        
+
         if (RGFW_isPressedI(win, RGFW_Right))
-            bodies[0].velocity.x += 0.005;
+            bodies[player].velocity.x += 0.005;
         else if (RGFW_isPressedI(win, RGFW_Left))
-            bodies[0].velocity.x += -0.005;
+            bodies[player].velocity.x += -0.005;
         else 
-            bodies[0].velocity.x = 0;
+            bodies[player].velocity.x = 0;
 
         if (RGFW_isPressedI(win, RGFW_Down))
-            bodies[0].velocity.y = 5;
+            bodies[player].velocity.y = 5;
         if (RGFW_isPressedI(win, RGFW_Up))
-            bodies[0].velocity.y = -2;    
+            bodies[player].velocity.y = -2;    
         
         RPhys_run();
 
